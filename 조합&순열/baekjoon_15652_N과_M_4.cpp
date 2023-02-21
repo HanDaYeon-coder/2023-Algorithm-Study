4a42a2abb1e97d493475d8f3b3f211f51e3574ca
HanDaYeon <75533232+HanDaYeon-coder@users.noreply.github.com>
2023-02-22 01:54:04 +0900

    Create baekjoon_15652_N과_M_4.cpp

---
+++ 조합&순열/baekjoon_15652_N과_M_4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int N=0;
+int M=0;
+int arr[9] = {0,};
+
+void combination(int cnt, int next){
+    if(cnt == M){
+        for(int i = 0; i<M; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    
+    for(int i=next; i<=N; i++){
+        arr[cnt] = i;
+        combination(cnt+1, i);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin>>N>>M;
+    combination(0,1);
+}
